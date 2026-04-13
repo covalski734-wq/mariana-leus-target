b190e07b228ad467a7ee8c7fe19b7576949d7590
Ilya Kovalevski <ilya.kovalevski@abfprogramming.com>
2026-04-13 17:50:35 +0200

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,20 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  
+  base: "./",
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+
   server: {
     port: 5173,
     open: true,
   },
+
   build: {
     outDir: 'dist',
     sourcemap: false,
